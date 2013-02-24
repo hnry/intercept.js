db88b4f847af2dafbe94104ae1022f0c408c41dd
hc <hheennrryy@gmail.com>
2013-02-24 03:00:14 -0500

    remove unneed line

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ if (typeof module !== undefined && module.exports) env = 'node';
 var layer = (env === 'node') ? require('layer') : this.layer;
 
 intercept = function (ctx, fn, interceptFn) {
-  var auto = auto || true;
   var result = false;
   var f = layer._find_context(ctx, fn, 0);
   layer.set(ctx, fn, function() {
