8b66bb270865d4800dadb528e08bda554cbed331
hc <hheennrryy@gmail.com>
2013-02-24 22:21:53 -0500

    working, 0.0.2

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ intercept = function (ctx, fn, interceptFn) {
   var f = layer._find_context(ctx, fn, 0);
   layer.set(ctx, fn, function() {
     result = { called: true, arguments: arguments};
-    if (auto) layer.unset(f[0][f[1]]);
     if (interceptFn && typeof interceptFn === 'function') interceptFn(result);
-    return layer.proxyStop;
+    layer.unset(f[0][f[1]]);
+    return new layer.Stop();
   });
+
   return function() {
     return result;
   };
